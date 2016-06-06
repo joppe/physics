99a60a74c901895ae06e7a3a04f0ea243e1a51bc
Joppe Aarts <joppe@apestaartje.info>
2016-06-06 22:54:24 +0200

    SizeInterface added

---
+++ ts/src/geometry/SizeInterface.ts
@@ -0,0 +1,18 @@
+/**
+ * @interface SizeInterface
+ */
+interface SizeInterface {
+    /**
+     * The width
+     *
+     * @type {number}
+     */
+    width:number;
+
+    /**
+     * The height
+     *
+     * @type {number}
+     */
+    height:number;
+}
