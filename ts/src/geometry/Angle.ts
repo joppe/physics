c56b99a713dced26b5bd5bc85bd84b87eda4b75b
Joppe Aarts <joppe@apestaartje.info>
2016-10-25 16:56:53 +0200

    added angle class

--- ts/src/geometry/Angle.ts
+++ ts/src/geometry/Angle.ts
@@ -1,21 +1,55 @@
-/**
- * Convert degrees to radians
- *
- * @param {number} degrees
- * @returns {number}
- */
-const degreesToRadians = (degrees:number):number => {
-    return degrees * (Math.PI / 180);
-};
+const
+    /**
+     * Convert degrees to radians
+     *
+     * @param {number} degrees
+     * @returns {number}
+     */
+    degreesToRadians = (degrees:number):number => {
+        return degrees * (Math.PI / 180);
+    },
+
+    /**
+     * Convert radians to degrees
+     *
+     * @param {number} radians
+     * @returns {number}
+     */
+    radiansToDegrees = (radians:number):number => {
+        return radians * (180 / Math.PI);
+    };
 
 /**
- * Convert radians to degrees
- *
- * @param {number} radians
- * @returns {number}
+ * @class Angle
  */
-const radiansToDegrees = (radians:number):number => {
-    return radians * (180 / Math.PI);
-};
+class Angle {
+    /**
+     * @type {number}
+     */
+    private _rad:number;
 
-export {degreesToRadians, radiansToDegrees};
+    /**
+     * @param {number} rad
+     */
+    constructor(rad:number) {
+        this._rad = rad;
+    }
+
+    /**
+     * @param {number} rad
+     * @returns {Angle}
+     */
+    static fromRadians(rad:number) {
+        return new Angle(rad);
+    }
+
+    /**
+     * @param {number} deg
+     * @returns {Angle}
+     */
+    static fromDegrees(deg:number) {
+        return new Angle(degreesToRadians(deg));
+    }
+}
+
+export {degreesToRadians, radiansToDegrees, Angle};
